98dd8e4078712566413223d0f689c7ec29571568
Probuddha Singha <probuddha23@gmail.com>
2016-08-25 04:14:39 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+	int n,c,d,i,j;
+	scanf("%d",&n);
+	c=n-1;
+	d=1;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=c;j++)
+		{
+			printf(" ");
+		}
+		c--;
+		for(j=1;j<=d;j++)
+		{
+			if(i>2)
+			{
+				if((j==1)||(j==d))
+				{
+					printf("* ");
+				}
+				else
+				{
+					printf("  ");
+				}
+			}
+			else
+				printf("* ");
+		}
+		d++;
+		printf("\n");
+	}
+	return 0;
+}
