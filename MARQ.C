3325073b2d19bf14c88001deeb4d8424eac08e80
Probuddha Singha <probuddha23@gmail.com>
2016-08-25 04:16:18 +0530

    Add files via upload

---
+++ MARQ.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+void main()
+{
+	int i;
+	for(i=1;i<=50&&(!kbhit());i++)
+	{
+		gotoxy(i,1);
+		printf("GIT");
+		delay(100);
+		clrscr();
+		if(i==50)
+		{
+			for(i=50;i>=1&&(!kbhit());i--)
+			{
+				gotoxy(i,1);
+				printf("GIT");
+				delay(100);
+				clrscr();
+			}
+		}
+	}
+	clrscr();
+}
---
+++ MARQUEE.C
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+void main()
+{
+     int a,i=0,j=0;
+     char str[50]="Thank you Dennis !!";
+     char f;
+     while(str[i]!='\0')
+     {
+		      i++;
+     }
+     str[i]=' ';
+     str[++i]='\0';
+     printf("%s",str);       //printf can print a string with spaces but scanf cannot input a string with spaces so we have to use gets()
+     delay(400);
+	     j=0;
+	     f=str[j];
+	     while(str[j]!='\0')
+	     {
+				str[j]=str[j+1];
+				j++;
+
+	     }
+	     str[--i]=f;
+	     str[++i]='\0';
+     while(1)
+     {
+
+	     clrscr();
+	     j=0;
+	     f=str[j];
+	     while(str[j]!='\0')
+	     {
+				str[j]=str[j+1];
+				j++;
+
+	     }
+	     str[i-1]=f;
+	     str[i]='\0';
+	     printf("%s",str);
+	     delay(400);
+     }
+     getch();
+}
